7f562c8e32aafd57f12b4c7348dd92ee7ddf4289
Jono Glassey <jonofilms@gmail.com>
2024-03-21 23:56:13 +0900

    Add theme transparency

--- lua/jono/plugins/lsp.lua
+++ lua/jono/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
             "williamboman/mason-lspconfig",
             "neovim/nvim-lspconfig",
         },
+        lazy = false,
         config = function()
             require("mason").setup()
             require("mason-lspconfig").setup({
@@ -14,14 +15,77 @@ return {
                     'lua_ls',
                     'clangd',
                     'marksman',
+                    'gopls',
                 },
             })
-            require('mason-lspconfig').setup_handlers({
-              function(server)
-                require("lspconfig")[server].setup({})
-              end,
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+            require("lspconfig").eslint.setup({})
+            require("lspconfig").tsserver.setup({})
+            require("lspconfig").clangd.setup({})
+            require("lspconfig").marksman.setup({})
+            require("lspconfig").gopls.setup({})
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
             })
         end,
+        keys = {
+            {
+                "<space>e",
+                "<cmd>lua vim.diagnostic.open_float{}<CR>",
+                desc = "Open Diagnostics"
+            },
+            {
+                "[d",
+                "<cmd>lua vim.diagnostic.goto_next{}<CR>",
+                desc = "Diagnostic - Prev"
+            },
+            {
+                "]d",
+                "<cmd>lua vim.diagnostic.goto_prev{}<CR>",
+                desc = "Diagnostic - Next"
+            },
+            {
+                "<space>f",
+                "<cmd>lua vim.diagnostic.setloclist{}<CR>",
+                desc = "Diagnostic - Next"
+            },
+        }
+
 
     },
 }
--- lua/jono/plugins/theme.lua
+++ lua/jono/plugins/theme.lua
@@ -3,7 +3,14 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        config = function()
+        opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                float = "transparent",
+            },
+        },
+        init = function()
             vim.cmd([[colorscheme tokyonight]])
         end,
     },
